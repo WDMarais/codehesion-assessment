e92c9e00be3dca2c178c11a2dc467e78a48adb44
Wynand Marais <marais.wynand@gmail.com>
2022-04-28 15:37:12 +0200

    Implement basic routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 import './App.css';
 import Header from './components/header';
 import Entry from './components/entry';
+import SignupForm from './components/signupForm';
+import LoginForm from './components/loginForm';
 
 function App() {
   return (
@@ -10,7 +17,13 @@ function App() {
       <header className="App-header">
         <Header />
       </header>
-      <Entry />
+      <Router>
+        <Routes>
+          <Route exact path="/signup" element={<SignupForm />}/>
+          <Route exact path="/login" element={<LoginForm />}/>
+          <Route path="/" element={<Entry />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
