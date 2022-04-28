ff3e84516ebae48398b99e051325da51ca1bff54
Wynand Marais <marais.wynand@gmail.com>
2022-04-28 10:07:49 +0200

    Add basic signup form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import './App.css';
 import Header from './components/header';
+import SignupForm from './components/signupForm';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <header className="App-header">
         <Header />
       </header>
+      <SignupForm />
     </div>
   );
 }
